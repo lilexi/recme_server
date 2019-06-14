2233555b477a8dd7bc2f872171ae0b1dbbe9176c
Sp <spherastree@gmail.com>
2019-06-15 01:55:45 +0300

    recme_server > add: db query

--- routes/index.js
+++ routes/index.js
@@ -7,88 +7,121 @@ const bodyParser = require('body-parser');
 router.use(cors());
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123',
-  database: 'recme'
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    database: 'recme'
 });
 
 connection.connect(function (err) {
-  (err)? console.log(err): console.log('[DB] Connection');
+    (err) ? console.log(err) : console.log('[DB] Connection');
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+router.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.use(cors());
 
-router.get('/posts', function(req, res) {
-  connection.query('SELECT * FROM content_db', function (err, data) {
-    (err)? res.send(err):res.json({posts :data});
-  })
+router.get('/posts', function (req, res) {
+    connection.query('SELECT * FROM content_db', function (err, data) {
+        (err) ? res.send(err) : res.json({posts: data});
+    })
 });
 
-router.get('/trands', function(req, res) {
-  connection.query('SELECT c.id, c.cat_info, c.`like`, c.Title FROM recme.content_db c, recme.trands t where c.id = t.p_id', function (err, data) {
-    (err)? res.send(err):res.json({trands :data});
-  })
+router.get('/trands', function (req, res) {
+    connection.query('SELECT c.id, c.cat_info, c.`like`, c.Title FROM recme.content_db c, recme.trands t where c.id = t.p_id', function (err, data) {
+        (err) ? res.send(err) : res.json({trands: data});
+    })
 });
 
-router.post('/getUserlike', function(req, res) {
-  // console.log(req.body.u_id.toString());
-  const u_id = req.body.u_id.toString();
-  connection.query(`select pl.id from u_like l, content_db pl where l.u_id = ${u_id} and pl.id = l.post_id`, function (err, data) {
-    (err)? res.send(err):res.json({like :data});
-  })
+router.post('/getUserlike', function (req, res) {
+    // console.log(req.body.u_id.toString());
+    const u_id = req.body.u_id.toString();
+    connection.query(`select pl.id from u_like l, content_db pl where l.u_id = ${u_id} and pl.id = l.post_id`, function (err, data) {
+        (err) ? res.send(err) : res.json({like: data});
+    })
 });
 
 app.use(
     bodyParser.urlencoded({
-      extended: true
+        extended: true
     })
 );
 
 app.use(bodyParser.json());
 
 router.post('/likeINSERT', function (req, res) {
-  // console.log(req.body);
-  connection.query(`INSERT INTO u_like (u_id,post_id) value (${req.body.u_id}, ${req.body.post_id})`, function (err, data) {
-    (err) ? res.send(err) : res.json({like :data});
-  });
+    // console.log(req.body);
+    connection.query(`INSERT INTO u_like (u_id,post_id) value (${req.body.u_id}, ${req.body.post_id})`, function (err, data) {
+        (err) ? res.send(err) : res.json({like: data});
+    });
 });
 
 router.post('/likeUPDATE', function (req, res) {
-  // console.log(req.body);
-  // console.log(`UPDATE content_db set \`like\` = ${req.body.like} where id = ${req.body.post_id}`);
-  // connection.query(`UPDATE content_db set \`like\` = ${req.body.like} where id = (select post_id from u_like where u_id = ${req.body.u_id} and post_id = ${req.body.post_id})`, function (err, data) {
-  //   (err) ? res.send(err) : res.json({like :data});
-  // });
-  connection.query(`UPDATE content_db c set c.\`like\` = ${req.body.like} where c.id = ${req.body.post_id}`, function (err, data) {
-    (err) ? res.send(err) : res.json({like :data});
-  });
+    // console.log(req.body);
+    // console.log(`UPDATE content_db set \`like\` = ${req.body.like} where id = ${req.body.post_id}`);
+    // connection.query(`UPDATE content_db set \`like\` = ${req.body.like} where id = (select post_id from u_like where u_id = ${req.body.u_id} and post_id = ${req.body.post_id})`, function (err, data) {
+    //   (err) ? res.send(err) : res.json({like :data});
+    // });
+    connection.query(`UPDATE content_db c set c.\`like\` = ${req.body.like} where c.id = ${req.body.post_id}`, function (err, data) {
+        (err) ? res.send(err) : res.json({like: data});
+    });
 });
 
 router.post('/likeDELETE', function (req, res) {
-  // console.log(req.body);
-  // console.log(`DELETE FROM u_like where u_id = ${req.body.u_id} and post_id = ${req.body.post_id}`);
-  connection.query(`DELETE FROM u_like where u_id = ${req.body.u_id} and post_id = ${req.body.post_id}`, function (err, data) {
-    (err) ? res.send(err) :res.json({like :data});
-  });
+    // console.log(req.body);
+    // console.log(`DELETE FROM u_like where u_id = ${req.body.u_id} and post_id = ${req.body.post_id}`);
+    connection.query(`DELETE FROM u_like where u_id = ${req.body.u_id} and post_id = ${req.body.post_id}`, function (err, data) {
+        (err) ? res.send(err) : res.json({like: data});
+    });
 });
 
-router.post('/postsID', function(req, res) {
-  console.log(req.body);
-  connection.query(`SELECT * FROM content_db where id = ${req.body.id}`, function (err, data) {
-    (err)? res.send(err):res.json({posts :data});
-  })
+router.post('/postsID', function (req, res) {
+    console.log(req.body);
+    connection.query(`SELECT * FROM content_db where id = ${req.body.id}`, function (err, data) {
+        (err) ? res.send(err) : res.json({posts: data});
+    })
+});
+
+
+
+router.post('/signup', (req, res) => {
+    connection.query(`INSERT into users (name, email, password) values (?)`, [req.body], function (err, data) {
+        console.log(err);
+        console.log(data);
+        (err) ? res.status(985)(err) : res.json({userID :data.insertId});
+    });
+});
+
+router.post('/getUser', function (req, res) {
+    console.log(req.body);
+    connection.query(`SELECT * FROM users where id = ${req.body.user_id}`, function (err, data) {
+        (err) ? res.send(err) : res.json({user: data});
+    })
+});
+
+router.post('/createPost', function (req, res) {
+    console.log(req.body.post);
+    connection.query(`INSERT into content_db (Title, text, cat_info, id_catigories, time_to_read, hard_rating, creator) values (?)`, [req.body.post], function (err, data) {
+        console.log(err);
+        console.log(data);
+        (err) ? res.status(err) : res.json({data});
+    });
+});
+
+router.post('/randomPost', function (req, res) {
+    console.log(req.body);
+    connection.query(`SELECT id FROM content_db where time_to_read = ${req.body.time} and id_catigories = '${req.body.cat}' order by \`like\` desc `, function (err, data) {
+        (err) ? res.send(err) : res.json({posts: data});
+    })
 });
 
 module.exports = router;
